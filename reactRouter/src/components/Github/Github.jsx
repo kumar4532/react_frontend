4dc74ca18820a29c246087f258d33792591db3dc
kumar4532 <krantikumar2001@gmail.com>
2024-03-30 00:53:37 +0530

    add react router project

---
+++ reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
+
+
+function Github() {
+    const data = useLoaderData()
+    /*const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch("https://api.github.com/users/kumar4532")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            setData(data)
+        })
+    }, [])*/
+
+    return (
+        <div className="text-center m-4 bg-gray-500 text-white">
+            Github Followers: {data.followers}
+            <img src={data.avatar_url} alt="Git picture" width={300} />
+        </div>
+    )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/kumar4532')
+    return response.json()
+};
