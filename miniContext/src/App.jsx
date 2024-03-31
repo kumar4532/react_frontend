fd92a989cd7c0218903ee0c1754ff519445a8db1
kumar4532 <krantikumar2001@gmail.com>
2024-03-31 20:21:57 +0530

    contaxt api project 1

---
+++ miniContext/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
+import Login from "./Components/login"
+import Profile from './Components/Profile'
+
+function App() {
+  
+
+
+  return (
+    <UserContextProvider>
+      <h1>React with chai</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
